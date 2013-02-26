a271fa254edbe865d949eab179c10e1d236a0093
Alex Nelson <ajnelson@cs.ucsc.edu>
2013-02-25 16:58:48 -0800

    Add necessary packages for Ubuntu 12.10

---
+++ deps/install_dependent_packages-ubuntu-12.10.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ -r /etc/debian_version ]; then
+  #Assume Ubuntu
+  sudo apt-get install \
+    autoconf \
+    g++ \
+    git \
+    libtool \
+    make
+fi
+#TODO
+#    afflib-tools \
+#    ewf-tools \
+#    libafflib-dev \
+#    libewf-dev \
+#    libexpat1-dev \
+#    libxml2-dev \
+#    libxml2-utils \
+#    python3 \
+#    python-dev \
+#    zlib1g-dev
