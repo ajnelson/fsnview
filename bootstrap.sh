af74cd22303ee9085418672dc789a4dc975a0874
Alex Nelson <ajnelson@cs.ucsc.edu>
2013-03-02 22:54:30 -0800

    While py360 uses dfxml, have bootstrap ensure the file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+set -e
+
+if [ ! -r deps/py360/dfxml.py ]; then
+  git submodule init deps/py360
+  git submodule update deps/py360
+  pushd deps/py360
+  git submodule init
+  git submodule update
+  popd
+fi
+
 autoreconf -i
