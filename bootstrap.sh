0957620d1aef0ce365622a5120cbd2f5bfbcb535
Alex Nelson <ajnelson@cs.ucsc.edu>
2013-03-11 15:22:20 -0700

    Update py360's time handling; remove DFXML dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,4 @@
 
 set -e
 
-if [ ! -r deps/py360/dfxml.py ]; then
-  git submodule init deps/py360
-  git submodule update deps/py360
-  pushd deps/py360
-  git submodule init
-  git submodule update
-  popd
-fi
-
 autoreconf -i
