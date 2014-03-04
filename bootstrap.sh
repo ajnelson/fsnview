c5c6aee5be9d78e62fdb7868fa0cef52a9112336
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-03-04 00:27:08 -0500

    Add UPartsFS to build process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 
+if [ -z "$prefix" ]; then
+  prefix="$PWD/build"
+fi
+
 set -e
 
-if [ ! -r deps/dfxml/python/dfxml.py -o ! -r deps/sleuthkit/configure.ac -o ! -r deps/py360/report360.py ]; then
+if [ ! -r deps/dfxml/python/dfxml.py -o ! -r deps/sleuthkit/configure.ac -o ! -r deps/py360/report360.py -o ! -r deps/upartsfs/configure.ac ]; then
   git submodule init
   git submodule sync
   git submodule update
 fi
+
+echo
+echo
+echo "Building and installing UPartsFS, assuming you're going to use the prefix '$prefix'."
+echo "If that's an incorrect assumption, run:"
+echo
+echo "    prefix=\$your_prefix $0"
+echo
+echo "This workaround is due to a strange build dependency that may be gone soon."
+echo
+echo
+
+pushd deps/upartsfs
+./bootstrap.sh
+./configure --disable-java --prefix="$prefix/share/fsnview/upartsfs"
+make -j
+make install
+popd
 
 aclocal
 automake --add-missing
